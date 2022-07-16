8c79f73f0004667d2f5f3e387fcd2b03930d4eff
kassiend_ <kas.dauren@gmail.com>
2022-07-16 20:13:13 +0600

    Add resources. Add nav graph.

--- app/src/main/java/com/example/githubrepos/data/modes/response/RepoResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.githubrepos.data.modes.response
-
-import com.example.githubrepos.data.modes.RepoEntity
-
-data class RepoResponse(
-    val totalCount: Int?,
-    val incompleteResults: Boolean?,
-    val items: List<RepoEntity>,
-)
