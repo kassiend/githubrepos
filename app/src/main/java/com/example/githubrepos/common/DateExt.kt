d3f7d4a8360bca47ffcbdb74a5f60799e2e31930
kassiend_ <kas.dauren@gmail.com>
2022-07-16 20:26:03 +0600

    Fix query date.

--- app/src/main/java/com/example/githubrepos/common/DateExt.kt
+++ app/src/main/java/com/example/githubrepos/common/DateExt.kt
@@ -4,7 +4,11 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun getLastMonthQuery() : String {
+    val cal = Calendar.getInstance()
+    cal.add(Calendar.DATE, -30)
+    val dateBefore30Days = cal.time
+
     val currentDate: String =
-        SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
-    return ("created:<$currentDate")
+        SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(dateBefore30Days)
+    return ("created:>$currentDate")
 }
